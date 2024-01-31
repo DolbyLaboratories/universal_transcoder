28c44cf4eb690130cea3c7aa4564d096405e08d8
Amaia <amaiasagastim@gmail.com>
2024-01-31 18:37:51 +0100

    correct example

--- ex4_transcoding_multichannelToAmbisonics.py
+++ ex4_transcoding_multichannelToAmbisonics.py
@@ -1,9 +1,8 @@
-# TRANSCODING OF AMBISONICS (WHAT ORDER?) TO MULTICHANNEL (WHAT CONFIGURATION?)
-
+# TRANSCODING MULTICHANNEL 5.1. to First order Ambisonics
 
 import numpy as np
 from universal_transcoder.auxiliars.get_cloud_points import (
-    get_equi_circumference_points,
+    get_equi_circumference_points,get_equi_t_design_points,
 )
 from universal_transcoder.auxiliars.get_input_channels import (
     get_input_channels_vbap, get_input_channels_ambisonics,
@@ -17,15 +16,6 @@ input_layout = MyCoordinates.mult_points(
     np.array(
         [
             (-120, 0, 1),
-            (0, 0, 1),
-            (120, 0, 1),
-        ]
-    )
-)
-output_layout = MyCoordinates.mult_points(
-    np.array(
-        [
-            (-120, 0, 1),
             (-30, 0, 1),
             (0, 0, 1),
             (30, 0, 1),
@@ -33,10 +23,23 @@ output_layout = MyCoordinates.mult_points(
         ]
     )
 )
+output_layout = MyCoordinates.mult_points(
+    np.array(
+        [
+            (-180, 0, 1),
+            (-90, 0, 1),
+            (0, -90, 1),
+            (0, 0, 1),
+            (0, 90, 1),
+            (90, 0, 1),
+        ]
+    )
+)
 
+#output_layout = get_equi_circumference_points(20, False)
 cloud_optimization = get_equi_circumference_points(36, False)
 input_matrix_optimization = get_input_channels_vbap(cloud_optimization, input_layout)
-cloud_plots = get_equi_circumference_points(360, False)
+cloud_plots = get_equi_circumference_points(360,False)
 input_matrix_plots = get_input_channels_vbap(cloud_plots, input_layout)
 
 # Transcoding to Ambisonics First order. Generate Dspk
@@ -54,9 +57,9 @@ dictionary = {
         "energy": 0,
         "radial_intensity": 0,
         "transverse_intensity": 0,
-        "pressure": 2,
-        "radial_velocity": 1,
-        "transverse_velocity": 1,
+        "pressure": 100,
+        "radial_velocity": 0.1,
+        "transverse_velocity": 0.1,
         "in_phase_quad": 0,
         "symmetry_quad": 0,
         "in_phase_lin": 0,
@@ -66,8 +69,8 @@ dictionary = {
     },
     "directional_weights": 1,
     "show_results": True,
-    "save_results": False,
-    "results_file_name": "ex3_50toFOA_pv",
+    "save_results": True,
+    "results_file_name": "ex4_2D_FOAencoding_pv",
     "input_matrix_plots": input_matrix_plots,
     "cloud_plots": cloud_plots,
 }
