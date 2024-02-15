a72284ab3c2f5f13f4cabfd416dd8bc8c390d862
Daniel Arteaga <daniel.arteaga@dolby.com>
2024-02-15 10:53:08 +0100

    Improved box plots

--- plots_davide/physics_boxplots.py
+++ plots_davide/physics_boxplots.py
@@ -1,51 +1,57 @@
+from pathlib import Path
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
-ut_txt_filename = "saved_results/paper_case2_ambi5OAto704_allrad_maxre/signal_data.txt"
-comp_txt_filename = (
-    "plots_davide/allrad_basic_renorm_zotter_n3d_mean/ambi_704_physics.txt"
-)
-
-columns = ["azimuth", "elevation", "p", "v_r", "v_t", "e", "i_r", "i_t"]
-ut_df = pd.read_csv(ut_txt_filename, names=columns, header=None)
-comp_df = pd.read_csv(comp_txt_filename, names=columns, header=None)
-
-combined_dfs = pd.DataFrame(
-    {
-        "ut": ut_df["p"],
-        "comp": comp_df["p"],
-    }
-)
-
-plt.figure()
 sns.set_style("white")
-sns.boxplot(data=combined_dfs, palette="viridis")
-plt.xlabel("encoding/decoding method")
-plt.ylabel("pressure")
-plt.show()
+COLUMNS = ["azimuth", "elevation", "P", "V_r", "V_t", "E", "I_r", "I_t"]
+PLOTS_PATH = Path(__file__).resolve().parents[1] / "saved_results"
 
-# example of...
-# putting things together
-cut_mask = ut_df["elevation"] > -0.5
-ut_df_cut = ut_df[cut_mask]
-cut_mask = comp_df["elevation"] > -0.5
-comp_df_cut = comp_df[cut_mask]
-combined_dfs = pd.DataFrame(
-    {
-        "ut pressure": ut_df_cut["p"],
-        "comp pressure": comp_df_cut["p"],
-        "ut energy": ut_df_cut["e"],
-        "comp energy": comp_df_cut["e"],
-        "ut intensity": ut_df_cut["i_r"],
-        "comp intensity": comp_df_cut["i_r"],
-    }
-)
 
-plt.figure()
-sns.set_style("white")
-sns.boxplot(data=combined_dfs, palette="viridis")
-plt.xlabel("encoding/decoding method")
-plt.ylabel("value")
-plt.show()
+def _get_path(name):
+    return PLOTS_PATH / name / "signal_data.txt"
+
+
+def box_plot(ut_txt_path, comp_txt_path, comp_name="comp", plot_type="pv"):
+
+    ut_df = pd.read_csv(ut_txt_path, names=COLUMNS, header=None)
+    ut_df["Method"] = "USAT"
+    comp_df = pd.read_csv(comp_txt_path, names=COLUMNS, header=None)
+    comp_df["Method"] = comp_name
+
+    combined_dfs = pd.concat([ut_df, comp_df])
+
+    # Selecting columns for the boxplot
+    if plot_type == "pv":
+        selected_columns = ["P", "V_r", "V_t"]
+    elif plot_type == "ei":
+        selected_columns = ["E", "I_r", "I_t"]
+    else:
+        raise ValueError("Wrong value for plot_type")
+
+    # Convert to longform
+    df_long = pd.melt(
+        combined_dfs,
+        id_vars="Method",
+        value_vars=selected_columns,
+        var_name="Quantity",
+        value_name="Value",
+    )
+
+    # Adding hue to the boxplot
+    sns.boxplot(
+        data=df_long, x="Quantity", y="Value", hue="Method"
+    )
+
+    # Display the plot
+    plt.show()
+
+
+if __name__ == "__main__":
+    box_plot(
+        _get_path("5OAdecoding714_USAT"),
+        _get_path("5OAdecoding714_allrad_maxre"),
+        plot_type="ei",
+        comp_name="AllRad"
+    )
