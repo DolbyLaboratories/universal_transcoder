08f4ba5ab95958b02f64989303aec47f0a84095a
Amaia <amaiasagastim@gmail.com>
2024-05-02 18:55:00 +0200

    license to scripts

--- paper/ex4_ObjectTo50.py
+++ paper/ex4_ObjectTo50.py
@@ -1,6 +1,5 @@
 """
 Copyright (c) 2024 Dolby Laboratories, Amaia Sagasti
-Copyright (c) 2023 Dolby Laboratories
 
 Redistribution and use in source and binary forms, with or without modification, are permitted 
 provided that the following conditions are met:
--- paper/plots_paper/panningPlots.py
+++ paper/plots_paper/panningPlots.py
@@ -1,3 +1,28 @@
+"""
+Copyright (c) 2024 Dolby Laboratories, Amaia Sagasti
+
+Redistribution and use in source and binary forms, with or without modification, are permitted 
+provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions 
+and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or 
+promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED 
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+POSSIBILITY OF SUCH DAMAGE.
+"""
+
 import warnings
 from pathlib import Path
 
@@ -120,7 +145,7 @@ def panning_plots_2d(
     plt.ylim(lim_min, lim_max)
     # Axis titles
     plt.xlabel("azimuth (deg)")
-    plt.ylabel("gain (linear)") # Set title for the x-axis
+    plt.ylabel("gain (linear)")  # Set title for the x-axis
     # Plot title
     plt.title(title)
     plt.grid()
@@ -130,9 +155,9 @@ def panning_plots_2d(
     # Save plots
     if type(path) is str:
 
-        name = "speaker_gains-linear-" + Path(path).stem  + ".png"
+        name = "speaker_gains-linear-" + Path(path).stem + ".png"
         save_plot(plt, path, name)
-        name = "speaker_gains-linear-" + Path(path).stem  + ".pdf"
+        name = "speaker_gains-linear-" + Path(path).stem + ".pdf"
         save_plot(plt, path, name)
     # Clear
     plt.clf()
@@ -166,9 +191,9 @@ def panning_plots_2d(
     ax.set_position([left, bottom, width, height])
     # Save plots
     if type(path) is str:
-        name = "speaker_gains-dB-" + Path(path).stem  + ".png"
+        name = "speaker_gains-dB-" + Path(path).stem + ".png"
         save_plot(plt, path, name)
-        name = "speaker_gains-dB-" + Path(path).stem  + ".pdf"
+        name = "speaker_gains-dB-" + Path(path).stem + ".pdf"
         save_plot(plt, path, name)
     # Clear
     plt.clf()
@@ -326,6 +351,4 @@ panning_plots_2d(
 )
 
 
-
 ############################################################################
-
